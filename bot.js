95e087607529aee0fe110af009148c70da5b6738
Erin McKean <erin@logocracy.com>
2016-01-23 22:46:05 -0600

    new bot

--- bot.js
+++ bot.js
@@ -4,28 +4,32 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+// This is the URL of a search for the latest tweets that include the phrase "for the record".
+var recordSearch = {q: '"for the record"', count: 10, result_type: "recent"}; 
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
+	T.get('search/tweets', recordSearch, function (error, data) {
 	  // log out any errors and responses
-	  console.log(error, data);
+	  // console.log(error, data);
 	  // If our search request to the server had no errors...
 	  if (!error) {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
+		var tweeter = data.statuses[0].user.screen_name
+		var tweetURL = "https://twitter.com/"+ tweeter+ "/status/" + retweetId;
+		var myTweet = "It's been noted for the record #fortherecord " + tweetURL;
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
+		// T.post('statuses/update/', { status: myTweet }, function (error, response) {
+		// 	if (response) {
+		// 		console.log('Success! Check your bot, it should have retweeted something.')
+		// 	}
+		// 	// If there was an error with our Twitter call, we print it out here.
+		// 	if (error) {
+		// 		console.log('There was an error with Twitter:', error);
+		// 	}
+		// })
+		console.log(myTweet);
 	  }
 	  // However, if our original search request had an error, we want to print it out here.
 	  else {
